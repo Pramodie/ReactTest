54e5059f5122061e8949b4a6888c4ab79198b58d
pramodie <adpramodie@gmail.com>
2021-12-06 00:06:30 +0530

    first commit

---
+++ WebAppApi/WebAppApi/Models/DCandidate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAppApi.Models
+{
+    public class DCandidate
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string name { get; set; }
+
+        [Column(TypeName = "nvarchar(12)")]
+        public string mobile { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string address { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string email { get; set; }
+    }
+}
---
+++ WebAppApi/WebAppApi/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAppApi.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
